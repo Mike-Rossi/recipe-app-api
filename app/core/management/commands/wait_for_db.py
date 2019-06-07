f4972a55b0845002e80b17a116826008f3d25697
Mike <michael@sinoc.io>
2019-06-07 21:55:08 +0100

    adds manage user endpoint

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -4,10 +4,11 @@ from django.db import connections
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     """Django command to pause execution until database is available"""
 
-    def handle(self, *args, **options): #what is run when we run this management command
+    def handle(self, *args, **options):  # what is run when we run this management command
         self.stdout.write('waiting for database...')
         db_conn = None
         while not db_conn:
